f83f512caab2fdea41e6e04d1bc0504c4bd9acd6
Lin07ux <linshengli.linux7@icloud.com>
2023-02-16 22:26:01 +0800

    feat(logic): 登录页面增加记住登录账户的功能

--- miniprogram/pages/login/login.ts
+++ miniprogram/pages/login/login.ts
@@ -1,10 +1,12 @@
+import { getStorageSync, setStorageSync, delStorageSync } from "../../services/storage"
 
 const user: IUser = getApp().globalData.user
+const REMEMBER_KEY: string = 'login_account_remember'
 
 Page({
   data: {
     form: {
-      account: '',
+      account: getStorageSync(REMEMBER_KEY) || '',
       password: '',
     },
     rules: [
@@ -17,12 +19,16 @@ Page({
       },
     ],
     error: '',
+    remember: true,
   },
   formInput(e: WechatMiniprogram.Input) {
     const { field } = e.currentTarget.dataset
     this.setData({
       [`form.${field}`]: e.detail.value
     })
+  },
+  rememberChange(e: WechatMiniprogram.CheckboxGroupChange) {
+    this.setData({ remember: !!e.detail.value.length });
   },
   submit() {
     this.selectComponent('#form').validate((valid: boolean, errors: Array<{ message: string }>) => {
@@ -37,6 +43,12 @@ Page({
           this.setData({ error: res.message || '登录失败' })
         } else {
           wx.showToast({ title: '登录成功' })
+
+          if (this.data.remember) {
+            setStorageSync(REMEMBER_KEY, this.data.form.account)
+          } else {
+            delStorageSync(REMEMBER_KEY)
+          }
 
           // 回退到前一页
           if (getCurrentPages().length > 1) {
