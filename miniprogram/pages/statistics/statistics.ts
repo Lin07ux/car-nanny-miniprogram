4e62f6ec3f3c202513b7eea26934e39e4f2424bd
Lin07ux <linshengli.linux7@icloud.com>
2023-04-22 10:10:00 +0800

    feat(page): 增加统计页面

---
+++ miniprogram/pages/statistics/statistics.ts
@@ -0,0 +1,26 @@
+import { shareHomePage } from '../../services/share'
+
+Page({
+  data: {
+
+  },
+  onLoad() {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+
+  },
+  onHide() {
+
+  },
+  onPullDownRefresh() {
+
+  },
+  onReachBottom() {
+
+  },
+  onShareAppMessage: shareHomePage,
+})
